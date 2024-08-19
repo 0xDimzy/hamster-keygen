4d79c882e03aacedc00438d14d95628c5cec0ebd
Diimzyy <40553463+Diimzyyy@users.noreply.github.com>
2024-08-19 09:23:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const EVENTS_DELAY = 20000
-    const MAX_KEYS_PER_GAME_PER_DAY = 10;
+    const EVENTS_DELAY = 20000;
+    const MAX_KEYS_PER_GAME_PER_DAY = 5;
+
     const games = {
         1: {
             name: 'Riding Extreme 3D',
             appToken: 'd28721be-fd2d-4b45-869e-9f253b554e50',
             promoId: '43e35910-c168-4634-ad4f-52fd764a843f',
-            eventsDelay: 21000,
+            eventsDelay: 20000,
             attemptsNumber: 22,
         },
         2: {
